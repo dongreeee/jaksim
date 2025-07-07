12be59b252410ae0470d9e50a001b9b7c9af7d67
hazel <wu1014@naver.com>
2025-07-07 21:54:29 +0900

    20250707 웹소켓 카운트 알림 작업 중

--- src/main/java/com/hazel/jaksim/websoket/MessageRepository.java
+++ src/main/java/com/hazel/jaksim/websoket/MessageRepository.java
@@ -1,7 +1,15 @@
 package com.hazel.jaksim.websoket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     long countByRecieveUserAndVc(String recieveUser, String vc);
+
+    @Modifying
+    @Query("UPDATE Message m SET m.vc = '1', m.recieve_reg = CURRENT_TIMESTAMP " +
+            "WHERE m.id = :id AND m.recieve_user = :username")
+    void updateReadStatus(@Param("id") Long id, @Param("username") String username);
 }
