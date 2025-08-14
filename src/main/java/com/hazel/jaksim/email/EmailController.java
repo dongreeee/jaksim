5d84afb6d667c6fed717cec6011a08cbeb81a656
ddongeunii <wu1014@naver.com>
2025-08-14 10:20:29 +0900

    20250814 EmailService.java

--- src/main/java/com/hazel/jaksim/email/EmailController.java
+++ src/main/java/com/hazel/jaksim/email/EmailController.java
@@ -1,17 +1,11 @@
 package com.hazel.jaksim.email;
 
-import com.hazel.jaksim.calendar.Calendar;
-import com.hazel.jaksim.member.Member;
-import com.hazel.jaksim.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,46 +13,14 @@ public class EmailController {
 
     private final EmailService emailService;
 
-    private final EmailRepository emailRepository;
-
-    private final MemberRepository memberRepository;
-
     @PostMapping("/send")
     public ResponseEntity<String>sendCode(@RequestParam String username) throws Exception {
-
-        Optional<Member> result = memberRepository.findByUsername(username);
-        if (result.isPresent()){
-            return ResponseEntity
-                    .status(HttpStatus.CONFLICT)  // 409: 중복
-                    .body("이미 존재하는 아이디입니다.");
-        }
-
-
-        String code = emailService.sendEmail(username);
-
-        Email email = new Email();
-        email.setUsername(username);
-        email.setAuthCode(code);
-        email.setCreatedAt(LocalDateTime.now());
-        email.setExpiredAt(LocalDateTime.now().plusMinutes(5));
-        emailRepository.save(email);
-
-        return ResponseEntity.ok("인증번호 전송");
+        return emailService.sendVerificationCode(username);
     }
 
     @PostMapping("/verify")
     public ResponseEntity<String>verify(@RequestParam String username, @RequestParam String code){
-        boolean result = emailService.verifyCode(username,code);
-        Optional<Email> optionalEmail = emailRepository.findTopByUsernameOrderByIdDesc(username);
-
-        if(result){
-            Email email = optionalEmail.get();
-            email.setVerified("1");
-            emailRepository.save(email);
-        }
-
-        return result ? ResponseEntity.ok("인증 성공") : ResponseEntity.badRequest().body("인증 실패");
-
+        return emailService.emailVerifyChk(username, code);
     }
 
 }
--- src/main/java/com/hazel/jaksim/email/EmailService.java
+++ src/main/java/com/hazel/jaksim/email/EmailService.java
@@ -1,13 +1,17 @@
 package com.hazel.jaksim.email;
 
-import jakarta.annotation.PostConstruct;
+import com.hazel.jaksim.member.Member;
+import com.hazel.jaksim.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -17,12 +21,38 @@ public class EmailService {
     @Autowired
     private final JavaMailSender javaMailSender;
 
+    private final MemberRepository memberRepository;
     private final EmailRepository emailRepository;
 //
 //    @PostConstruct
 //    public void checkRepo() {
 //        System.out.println("📌 EmailRepository 주입됨? " + (emailRepository != null));
 //    }
+
+
+
+
+    public ResponseEntity<String> sendVerificationCode(String username) throws Exception{
+
+//      1. 회원 중복 확인
+        if (memberRepository.findByUsername(username).isPresent()){
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)  // 409: 중복
+                    .body("이미 존재하는 아이디입니다.");
+        }
+//      2. 메일 발송
+        String code = sendEmail(username);
+
+//      3. 인증 정보 저장
+        Email email = new Email();
+        email.setUsername(username);
+        email.setAuthCode(code);
+        email.setCreatedAt(LocalDateTime.now());
+        email.setExpiredAt(LocalDateTime.now().plusMinutes(5));
+        emailRepository.save(email);
+
+        return ResponseEntity.ok("인증번호 전송");
+    }
 
     public String sendEmail(String toEmail){
         String authCode = generateCode();
@@ -42,6 +72,19 @@ public class EmailService {
         return String.valueOf(number);
     }
 
+    public ResponseEntity<String>emailVerifyChk(String username, String code){
+        boolean result = verifyCode(username,code);
+        Optional<Email> optionalEmail = emailRepository.findTopByUsernameOrderByIdDesc(username);
+
+        if(result){
+            Email email = optionalEmail.get();
+            email.setVerified("1");
+            emailRepository.save(email);
+        }
+
+        return result ? ResponseEntity.ok("인증 성공") : ResponseEntity.badRequest().body("인증 실패");
+    }
+
     public boolean verifyCode(String username, String inputCode) {
         Email auth = emailRepository.findTopByUsernameOrderByIdDesc(username)
                 .orElseThrow(() -> new RuntimeException("인증 요청 없음"));
@@ -52,6 +95,8 @@ public class EmailService {
 
         return auth.getAuthCode().equals(inputCode);
     }
+
+
 
 
 }
